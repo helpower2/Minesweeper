b0e23074e1c35a79afa76a654fd987eee21f526e
helpower2 <jeffreymonnink@gmail.com>
2019-06-24 09:41:48 +0200

    update to coc and unity

---
+++ unity/Minesweeper/Assets/Scripts/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public static class Extensions
+{
+    // Generic
+    public static void DestroyChilds(this Transform t)
+    {
+        for (int i = t.childCount - 1; i >= 0; i--)
+            MonoBehaviour.Destroy(t.GetChild(i).gameObject);
+    }
+
+    public static void ChildsActiveState(this Transform t, bool state)
+    {
+        for (int i = t.childCount - 1; i >= 0; i--)
+            t.GetChild(i).gameObject.SetActive(state);
+    }
+
+    public static void Reset(this Transform t)
+    {
+        t.localPosition = Vector3.zero;
+        t.localEulerAngles = Vector3.zero;
+        t.localScale = Vector3.one;
+    }
+
+    // Spriterenderer
+    public static void SetColor(this SpriteRenderer spriteRenderer, Color color)
+    {
+        spriteRenderer.color = color;
+    }
+
+    public static void SetSprite(this SpriteRenderer spriteRenderer, Sprite sprite)
+    {
+        spriteRenderer.sprite = sprite;
+    }
+}
---
+++ unity/Minesweeper/Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T _instance { get; private set; }
+
+    public static T Instance()
+    {
+        if (_instance == null)
+        {
+            _instance = FindObjectOfType<T>();
+        }
+
+        return _instance;
+    }
+}
